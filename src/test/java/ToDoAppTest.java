c1f9491a591a54571216b3995cf860853270520f
Tsvetozar Bonev <tsbonev@gmail.com>
2018-05-28 12:07:50 +0300

    Complete skeleton of structure

---
+++ src/test/java/ToDoAppTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
+public class ToDoAppTest {
+
+    @Test
+    public void initialTest(){
+
+        assertThat(true, is(true));
+
+    }
+
+}
